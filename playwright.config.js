9f55e5751f23c25304d9ca080988c5fce84495ff
dss539 <dss539@gmail.com>
2026-07-17 16:47:43 -0400

    Configure Playwright browser tests

---
+++ playwright.config.js
@@ -0,0 +1,20 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  retries: 1,
+  use: {
+    baseURL: 'http://127.0.0.1:4173',
+    trace: 'retain-on-failure'
+  },
+  webServer: {
+    command: 'python3 -m http.server 4173',
+    url: 'http://127.0.0.1:4173',
+    reuseExistingServer: false
+  },
+  projects: [
+    { name: 'desktop', use: { ...devices['Desktop Chrome'] } },
+    { name: 'mobile', use: { ...devices['Pixel 7'] } }
+  ]
+});
